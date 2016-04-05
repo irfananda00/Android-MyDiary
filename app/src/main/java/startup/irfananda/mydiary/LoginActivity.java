03efa7cd4ed4e460dd04f2c1c08462619cbcd10c
irfananda <irfanRsatrio@gmail.com>
2016-04-05 16:58:26 +0700

    free sign in

--- app/src/main/java/startup/irfananda/mydiary/LoginActivity.java
+++ app/src/main/java/startup/irfananda/mydiary/LoginActivity.java
@@ -212,6 +212,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Shows the progress UI and hides the login form.
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+
     private void showProgress(final boolean show) {
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
         // for very easy animations. If available, use these APIs to fade-in
